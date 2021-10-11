4560d10bfdcd9f48ee9eeea5040b9681ebc1d725
GeorgeMoraru <11480344+GeorgeMoraru@users.noreply.github.com>
2021-10-12 00:32:27 +0300

    Restart
    Cleanup

--- core/js/app.js
+++
@@ -1,7 +0,0 @@
-import { getPageResources, getCSSResources, getJSResources, getWidgets, getWidgetSettings } from '/pageSettings.js';
-import { LoadCSS } from '/core/js/loader/cssLoader.js';
-//import '/core/libs/jQuery.js';
-//import '/core/libs/underscore.js';
-import { LoadJS } from '/core/js/loader/jsLoader.js';
-LoadCSS(getCSSResources());
-LoadJS(getJSResources());

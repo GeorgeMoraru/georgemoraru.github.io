3c291d7888a50efa93e34f3603a8cd028999ebf3
GeorgeMoraru <11480344+GeorgeMoraru@users.noreply.github.com>
2022-05-04 00:03:15 +0300

    fix

--- js/base.js
+++ js/base.js
@@ -4,19 +4,3 @@ var randomHeroImage = function(){
     var heroImage = document.querySelector("#heroImg");
     heroImage.src = images[randomNumber];
 }
-
-var initMap = function() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
-    });
-}
-

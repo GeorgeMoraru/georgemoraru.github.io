76ec35ff5e2b85eb33bc87b710a584c82d895479
GeorgeMoraru <11480344+GeorgeMoraru@users.noreply.github.com>
2022-05-10 00:13:09 +0300

    update

--- js/base.js
+++ js/base.js
@@ -1,6 +1,6 @@
 var randomHeroImage = function(){
-    var images = ["/img/hero1.jpg", "/img/hero2.jpg", "/img/hero3.jpg", "/img/hero4.jpg", "/img/hero5.jpg"];
-    var randomNumber = Math.floor((Math.random() * 5));
+    var images = ["/img/hero1.jpg", "/img/hero2.jpg", "/img/hero3.jpg", "/img/hero4.jpg", "/img/hero5.jpg", "/img/hero6.jpg"];
+    var randomNumber = Math.floor((Math.random() * 6));
     var heroImage = document.querySelector("#heroImg");
     heroImage.src = images[randomNumber];
 }

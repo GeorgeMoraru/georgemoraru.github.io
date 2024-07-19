bd211f1d2d4c526921b03a8c1fb2c705096309bc
GeorgeM <geoege.moraru.1990@gmail.com>
2024-07-19 19:49:03 +0000

    changes to scroll

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("wheel", function() {
+    var sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        if(section.className.indexOf("active") > -1) {
+            section.className = section.className.replace(" active", "");
+        } else {
+            section.className = section.className + " active";
+            section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+        }
+    });
+})
